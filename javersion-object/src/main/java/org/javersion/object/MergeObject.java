9e4e928fdb6202a059e09b8a5b075a0d464b038d
Samppa Saarela <samppa.saarela@iki.fi>
2015-11-10 20:22:08 +0200

    Getters for relevant data

--- javersion-object/src/main/java/org/javersion/object/MergeObject.java
+++ javersion-object/src/main/java/org/javersion/object/MergeObject.java
@@ -1,7 +1,14 @@
 package org.javersion.object;
 
+import java.util.Set;
+
 import org.javersion.core.Merge;
+import org.javersion.core.Revision;
+import org.javersion.core.VersionProperty;
 import org.javersion.path.PropertyPath;
+import org.javersion.util.Check;
+
+import com.google.common.collect.Multimap;
 
 public class MergeObject<T, M> {
 
@@ -11,7 +18,19 @@ public class MergeObject<T, M> {
 
     public MergeObject(T object, Merge<PropertyPath, Object, M> merge) {
         this.object = object;
-        this.merge = merge;
+        this.merge = Check.notNull(merge, "merge");
+    }
+
+    public T getObject() {
+        return object;
+    }
+
+    public Set<Revision> getMergeHeads() {
+        return merge.getMergeHeads();
+    }
+
+    public Multimap<PropertyPath, VersionProperty<Object>> getConflicts() {
+        return merge.getConflicts();
     }
 
 }
