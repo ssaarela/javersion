0a11d27411cfe86a074236579088f79cc1fca162
Samppa Saarela <samppa.saarela@iki.fi>
2016-01-26 20:38:53 +0200

    Functional set key mapping: @SetKey(by=Class<? extends Function>)

--- javersion-object/src/main/java/org/javersion/object/SetKey.java
+++ javersion-object/src/main/java/org/javersion/object/SetKey.java
@@ -21,12 +21,23 @@ import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
+import java.util.function.Function;
 
 @Target({ FIELD, METHOD, TYPE })
 @Retention(RUNTIME)
 @Documented
+@Inherited
 public @interface SetKey {
-    String[] value();
+
+    String[] value() default {};
+
+    Class<? extends Function<?, ?>> by() default None.class;
+
+    abstract class None implements Function<Object, Object> {
+        private None() {}
+    }
+
 }
