0e6bcd4804c0ff0bd7019bdac4cf459c9eafeeee
Samppa Saarela <samppa.saarela@iki.fi>
2016-04-20 14:25:05 +0300

    Optimize Revision parser and toString

--- javersion-core/src/main/java/org/javersion/core/Revision.java
+++ javersion-core/src/main/java/org/javersion/core/Revision.java
@@ -2,11 +2,10 @@ package org.javersion.core;
 
 import static java.lang.System.currentTimeMillis;
 
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicLong;
 
 import javax.annotation.concurrent.Immutable;
-
-import org.javersion.util.BinaryEncoder;
 
 import com.eaio.uuid.UUIDGen;
 
@@ -20,11 +19,13 @@ import com.eaio.uuid.UUIDGen;
  * sequence (16-bit): running sequence number within same millisecond
  * node (64-bit): "The current clock and node value" from com.eaio.uuid.UUIDGen.clockSeqAndNode
  * </pre>
+ *
+ * String representation of a Revision uses Base32 Crockford characters and is
+ * in it's canonical form (all upper case base characters) lexically comparable.
+ * Creating a Revision from a String utilizes Crockford's alias characters.
  */
 @Immutable
 public final class Revision implements Comparable<Revision> {
-
-    private static final BinaryEncoder ENCODER = BinaryEncoder.BASE32_CROCKFORD_NUMBER;
 
     public static final Revision MIN_VALUE = new Revision(0, 0);
 
@@ -82,10 +83,7 @@ public final class Revision implements Comparable<Revision> {
 
     @Override
     public String toString() {
-        return new StringBuilder(27)
-                .append(ENCODER.encodeLong(timeSeq))
-                .append('-')
-                .append(ENCODER.encodeLong(node)).toString();
+        return toString(timeSeq) + "-" + toString(node);
     }
 
     public static int compareUnsigned(long x, long y) {
@@ -113,4 +111,97 @@ public final class Revision implements Comparable<Revision> {
             }
         }
     }
+
+    public static long toLong(String str) {
+        try {
+            return toNumber(str.charAt(12))
+                    | toNumber(str.charAt(11)) << 5
+                    | toNumber(str.charAt(10)) << 10
+                    | toNumber(str.charAt(9)) << 15
+                    | toNumber(str.charAt(8)) << 20
+                    | toNumber(str.charAt(7)) << 25
+                    | toNumber(str.charAt(6)) << 30
+                    | toNumber(str.charAt(5)) << 35
+                    | toNumber(str.charAt(4)) << 40
+                    | toNumber(str.charAt(3)) << 45
+                    | toNumber(str.charAt(2)) << 50
+                    | toNumber(str.charAt(1)) << 55
+                    | toNumber(str.charAt(0)) << 60;
+        } catch (IndexOutOfBoundsException e) {
+            throw new IllegalArgumentException("Expected 13 character String, got " + str);
+        }
+    }
+
+    public static String toString(long val) {
+        return new String(new char[] {
+                CHARS[MASK & (int) (val >>> 60)],
+                CHARS[MASK & (int) (val >>> 55)],
+                CHARS[MASK & (int) (val >>> 50)],
+                CHARS[MASK & (int) (val >>> 45)],
+                CHARS[MASK & (int) (val >>> 40)],
+                CHARS[MASK & (int) (val >>> 35)],
+                CHARS[MASK & (int) (val >>> 30)],
+                CHARS[MASK & (int) (val >>> 25)],
+                CHARS[MASK & (int) (val >>> 20)],
+                CHARS[MASK & (int) (val >>> 15)],
+                CHARS[MASK & (int) (val >>> 10)],
+                CHARS[MASK & (int) (val >>> 5)],
+                CHARS[MASK & (int) val]
+        });
+    }
+
+    private static long toNumber(char ch) {
+        try {
+            long number = NUMBERS[ch];
+            if (number < 0) {
+                throw new IllegalArgumentException("Unrecognized character: " + ch);
+            }
+            return number;
+        } catch (IndexOutOfBoundsException e) {
+            throw new IllegalArgumentException("Unrecognized character: " + ch);
+        }
+    }
+
+    private static final int MASK = 0b11111;
+
+    private static final char[] CHARS = "0123456789ABCDEFGHJKMNPQRSTVWXYZ".toCharArray();
+
+    private static final int[] NUMBERS = new int['z' + 1];
+
+    static {
+        Arrays.fill(NUMBERS, -1);
+        NUMBERS['0'] = NUMBERS['O'] = NUMBERS['o'] = 0;
+        NUMBERS['1'] = NUMBERS['I'] = NUMBERS['i'] = NUMBERS['L'] = NUMBERS['l'] = 1;
+        NUMBERS['2'] = 2;
+        NUMBERS['3'] = 3;
+        NUMBERS['4'] = 4;
+        NUMBERS['5'] = 5;
+        NUMBERS['6'] = 6;
+        NUMBERS['7'] = 7;
+        NUMBERS['8'] = 8;
+        NUMBERS['9'] = 9;
+        NUMBERS['A'] = NUMBERS['a'] = 10;
+        NUMBERS['b'] = NUMBERS['B'] = 11;
+        NUMBERS['C'] = NUMBERS['c'] = 12;
+        NUMBERS['D'] = NUMBERS['d'] = 13;
+        NUMBERS['E'] = NUMBERS['e'] = 14;
+        NUMBERS['F'] = NUMBERS['f'] = 15;
+        NUMBERS['G'] = NUMBERS['g'] = 16;
+        NUMBERS['H'] = NUMBERS['h'] = 17;
+        NUMBERS['J'] = NUMBERS['j'] = 18;
+        NUMBERS['K'] = NUMBERS['k'] = 19;
+        NUMBERS['M'] = NUMBERS['m'] = 20;
+        NUMBERS['N'] = NUMBERS['n'] = 21;
+        NUMBERS['P'] = NUMBERS['p'] = 22;
+        NUMBERS['Q'] = NUMBERS['q'] = 23;
+        NUMBERS['R'] = NUMBERS['r'] = 24;
+        NUMBERS['S'] = NUMBERS['s'] = 25;
+        NUMBERS['T'] = NUMBERS['t'] = 26;
+        NUMBERS['V'] = NUMBERS['v'] = 27;
+        NUMBERS['W'] = NUMBERS['w'] = 28;
+        NUMBERS['X'] = NUMBERS['x'] = 29;
+        NUMBERS['Y'] = NUMBERS['y'] = 30;
+        NUMBERS['Z'] = NUMBERS['z'] = 31;
+    }
+
 }
