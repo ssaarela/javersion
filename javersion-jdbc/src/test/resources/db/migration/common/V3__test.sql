75df57fa409b6f1a13df3db7692ced6a45b4b11c
Samppa Saarela <samppa.saarela@iki.fi>
2016-05-10 16:43:15 +0300

    Add index to speed up tests

--- javersion-jdbc/src/test/resources/db/migration/common/V3__test.sql
+++ javersion-jdbc/src/test/resources/db/migration/common/V3__test.sql
@@ -3,3 +3,5 @@ alter table DOCUMENT_VERSION add column NAME varchar(255);
 
 alter table ENTITY add column NAME varchar(255);
 alter table ENTITY_VERSION add column COMMENT varchar(255);
+
+create index ENTITY_VERSION_PROPERTY_STATUS_IDX on ENTITY_VERSION_PROPERTY (STATUS);
